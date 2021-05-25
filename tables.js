c2f93e3ce2eb7e0e1d277295b0e16e24596ce6cb
Kushal-Goenka <kushalgoenka97@gmail.com>
2021-05-25 09:32:01 -0500

    tables.js without express

--- tables.js
+++ tables.js
@@ -1,8 +1,3 @@
-const express = require('express'); // Adding Express
-const app = express(); // Initializing Express
-
-
-
 const puppeteer = require('puppeteer');
 const moment = require('moment-timezone');
 const { readFile, writeFile } = require('fs').promises;
@@ -119,147 +114,121 @@ async function scrapeInfiniteScrollItems(
     }
   }
 
-  app.get('/', function(req, res) {
-    // Sending 'Test' back to Postman
-    // https://stackoverflow.com/questions/63199136/sending-csv-back-with-express
 
 
-    (async () => {
+(async () => {
+  // const browser = await puppeteer.launch({ executablePath: 'Chromium',headless: true,dumpio: false});
+  const browser = await puppeteer.launch({ executablePath: 'puppeteer/.local-chromium/mac-869685/chrome-mac/Chromium.app/Contents/MacOS/Chromium',headless: true,dumpio: false});
+// const browser = await puppeteer.launch({headless: true,dumpio: false});
+// const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://thefly.com/news.php');
+//   await page.screenshot({path: 'output.png'});
     
-      //   const browser = await puppeteer.launch({ executablePath: 'puppeteer/.local-chromium/mac-869685/chrome-mac/Chromium.app/Contents/MacOS/Chromium',headless: true,dumpio: false});
-      const browser = await puppeteer.launch({args: [
-          '--no-sandbox',
-          '--no--zygote',
-          "--single-process",
-          "--disable-dev-shm-usage",
-        ],headless: true,defaultViewport: null,});
-      // const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://thefly.com/news.php');
-      //   await page.screenshot({path: 'output.png'});
-          
-          // console.log("HERE");
-      
-          // page.evaluate(() => console.log('hello', 5));
-      
-      
-          const items = await scrapeInfiniteScrollItems(page, getData);
-      
-          const [data1,time,date] = await page.evaluate(getData);
-      
-          // console.log(time,date)
-      
-          // console.log(data1.length);
-      
-          // https://stackoverflow.com/questions/35974976/json-group-by-count-output-to-key-value-pair-json-result
-      
-          var occurences = data1.reduce(function (r, row) {
-              r[row.ticker] = ++r[row.ticker] || 1;
-              // r[row.ticker] = ++r[row.ticker];
-              return r;
-          }, {});
-          // Test
-          console.log("Occurences:",occurences);
-          
-          var result = Object.keys(occurences).map(function (key) {
-              if(occurences[key] >= 3){
-                  return { ticker: key, Count: occurences[key] };
-              } else{
-                  return;
-              }
-              
-          });
-          
-          // https://stackoverflow.com/questions/24806772/how-to-skip-over-an-element-in-map
-          result = result.filter(function(element){
-              if(element == null){
-                  return false;
-              }
-              else{
-                  return true;
-              }
-      
-          }).map(function(element){
-              return element;
-          });
-      
-          
-          // var result = Object.keys(occurences).reduce(function(key) {
-          //     if(occurences[key] >= 3){
-          //         return { key: key, value: occurences[key] };
-          //     } else{
-          //         return;
-          //     }
-          // },[]);
-      
-          console.log(result);
-          // console.log(result.length);
-      
-      
-      
-          // var combined = result.map(x => Object.assign(x, data1.find(y => y.ticker == x.ticker)));
-      
-          // console.log(combined);
-      
-          // var matches = data1.filter(item =>
-          //   filterParams.every(paramItem =>
-          //     item[paramItem.param] === paramItem.value));
-      
-          var tickers = []
-          for(const symbol of result){
-            tickers.push(symbol.ticker);
-          }
-      
-          console.log(tickers);
-      
-        //   var combined = result.filter(function(element){
-        //     console.log(element.ticker);
-        //     if(element.ticker in tickers){
-        //         return true;
-        //     }
-        //     else{
-        //         return false;
-        //     }
-      
-        // }).map(function(element){
-        //     return element;
-        // });
-      
-        var combined = data1.filter(item => tickers.includes(item.ticker));
-      
-      
-          // console.log(combined);
-      
-        combined = combined.map(x => Object.assign(x, result.find(y => y.ticker == x.ticker)));
+    // console.log("HERE");
+
+    // page.evaluate(() => console.log('hello', 5));
+
+
+    const items = await scrapeInfiniteScrollItems(page, getData);
+
+    const [data1,time,date] = await page.evaluate(getData);
+
+    // console.log(time,date)
+
+    // console.log(data1.length);
+
+    // https://stackoverflow.com/questions/35974976/json-group-by-count-output-to-key-value-pair-json-result
+
+    var occurences = data1.reduce(function (r, row) {
+        r[row.ticker] = ++r[row.ticker] || 1;
+        // r[row.ticker] = ++r[row.ticker];
+        return r;
+    }, {});
+    // Test
+    console.log("Occurences:",occurences);
+    
+    var result = Object.keys(occurences).map(function (key) {
+        if(occurences[key] >= 3){
+            return { ticker: key, Count: occurences[key] };
+        } else{
+            return;
+        }
         
-      
-        combined = combined.sort((a, b) => (a.ticker > b.ticker) ? 1 : -1);
-        console.log(combined);
-      
-      
-          // console.log(data1);
-          const CSV = arrayToCSV(combined);
-          await writeCSV("output.csv", CSV);
-      
-      
-        await browser.close();
-      
-        // https://dev.to/waqasabbasi/building-a-search-engine-api-with-node-express-and-puppeteer-using-google-search-4m21
-        // https://dev.to/heyshadowsmith/how-to-make-an-api-from-scraped-data-using-express-puppeteer-2n7e
-          
+    });
+    
+    // https://stackoverflow.com/questions/24806772/how-to-skip-over-an-element-in-map
+    result = result.filter(function(element){
+        if(element == null){
+            return false;
+        }
+        else{
+            return true;
+        }
 
-        res.type('text/csv');
-        res.attachment('thefly.csv');
-        res.send(CSV);
-      })();
+    }).map(function(element){
+        return element;
+    });
+
+    
+    // var result = Object.keys(occurences).reduce(function(key) {
+    //     if(occurences[key] >= 3){
+    //         return { key: key, value: occurences[key] };
+    //     } else{
+    //         return;
+    //     }
+    // },[]);
+
+    console.log(result);
+    // console.log(result.length);
 
 
 
-});
+    // var combined = result.map(x => Object.assign(x, data1.find(y => y.ticker == x.ticker)));
+
+    // console.log(combined);
+
+    // var matches = data1.filter(item =>
+    //   filterParams.every(paramItem =>
+    //     item[paramItem.param] === paramItem.value));
+
+    var tickers = []
+    for(const symbol of result){
+      tickers.push(symbol.ticker);
+    }
+
+    console.log(tickers);
+
+  //   var combined = result.filter(function(element){
+  //     console.log(element.ticker);
+  //     if(element.ticker in tickers){
+  //         return true;
+  //     }
+  //     else{
+  //         return false;
+  //     }
+
+  // }).map(function(element){
+  //     return element;
+  // });
+
+  var combined = data1.filter(item => tickers.includes(item.ticker));
 
 
+    // console.log(combined);
+
+  combined = combined.map(x => Object.assign(x, result.find(y => y.ticker == x.ticker)));
+  
+
+  combined = combined.sort((a, b) => (a.ticker > b.ticker) ? 1 : -1);
+  console.log(combined);
 
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+    // console.log(data1);
+    const CSV = arrayToCSV(combined);
+  	await writeCSV("output.csv", CSV);
+
+
+  await browser.close();
+
+})();
